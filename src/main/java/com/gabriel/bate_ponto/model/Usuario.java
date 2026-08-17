5d3d8d32ed5c96a1da67ca3ef526fe67a0ec1267
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-17 20:49:07 -0300

    - cria o package da model. - cria a Entity do usuario.

---
+++ src/main/java/com/gabriel/bate_ponto/model/Usuario.java
@@ -0,0 +1,17 @@
+package com.gabriel.bate_ponto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String senha;
+    private String nome;
+}
