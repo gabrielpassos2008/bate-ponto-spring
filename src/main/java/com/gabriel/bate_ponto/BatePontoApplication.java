ec96f917714061ec17315573e724fe6d20c3355d
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-17 20:43:29 -0300

    primeiro commit

---
+++ src/main/java/com/gabriel/bate_ponto/BatePontoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.bate_ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/bate_ponto/BatePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.bate_ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
