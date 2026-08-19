adc20d862958421a5f287035332a3d65cd6a2e8d
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-19 20:37:14 -0300

    - Cria no método aonde vai retornar o usuário pelo Email. No UsuarioRepository.

--- src/main/java/com/gabriel/bate_ponto/repository/UsuarioRepository.java
+++ src/main/java/com/gabriel/bate_ponto/repository/UsuarioRepository.java
@@ -4,6 +4,10 @@ import com.gabriel.bate_ponto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Long, Usuario> {
+
+    Optional<Usuario> findByEmail(String email);
 }
