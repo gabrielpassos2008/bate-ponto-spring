09a84bbb1cd3e5bf86d0095261c3b9fd919d0c48
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-21 13:24:24 -0300

    - cria o doFilterInternal no SecurityConfig.

--- src/main/java/com/gabriel/bate_ponto/security/SecurityFilter.java
+++ src/main/java/com/gabriel/bate_ponto/security/SecurityFilter.java
@@ -8,6 +8,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -23,7 +27,25 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.extrairToken(request); // extrai o token da requisição
+        try {
+            if (token != null){
+                String email = tokenService.validarToken(token); //retorna o email do token da requisição
+                UserDetails userDetails = usuarioRepository.findByEmail(email). //consulta no banco o email
+                        orElseThrow(()-> new UsernameNotFoundException("Usuerio nao encontrado")); // valia se existe
 
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken
+                        (userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                // cria a autenticação
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken); // registra o login do usuario
+            }
+            filterChain.doFilter(request,response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
