813e50adfae81e0ee64580a6bca5b035926ed949
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-24 20:26:49 -0300

    - Cria a AdministradorService. - Cria o registrarUsuário.

---
+++ src/main/java/com/gabriel/bate_ponto/Service/AdministradorService.java
@@ -0,0 +1,31 @@
+package com.gabriel.bate_ponto.Service;
+
+import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
+import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
+import com.gabriel.bate_ponto.model.Usuario;
+import com.gabriel.bate_ponto.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdministradorService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UsuarioResponseDTO registrarUsuario(UsuarioCreateDTO dto){
+        Usuario novo = new Usuario();
+        novo.setNome(dto.nome());
+        novo.setEmail(dto.email());
+        novo.setSenha(passwordEncoder.encode(dto.senha()));
+        novo.setAtivo(dto.ativo());
+        novo.setRole(dto.role());
+
+        this.usuarioRepository.save(novo);
+
+        return new UsuarioResponseDTO(novo.getEmail(), novo.getNome(), novo.isAtivo(),novo.getRole());
+    }
+}
