7d682d47e5bc67d11bcc38b8766c9f63109563fe
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-24 20:27:32 -0300

    - Cria o UsuarioController. - Cria o método post aonde vai registrar o Usuário.

--- src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
@@ -1,7 +1,29 @@
 package com.gabriel.bate_ponto.controller;
 
+import com.gabriel.bate_ponto.Service.AdministradorService;
+import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
+import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ponto")
 public class UsuarioController {
+
+    @Autowired
+    private AdministradorService administradorService;
+
+    @PostMapping("/registrar/adm")
+    public ResponseEntity<UsuarioResponseDTO> postRegistrarAdm(@RequestBody UsuarioCreateDTO dto){
+        UsuarioResponseDTO usuario = administradorService.registrarUsuario(dto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(usuario);
+    }
 }
