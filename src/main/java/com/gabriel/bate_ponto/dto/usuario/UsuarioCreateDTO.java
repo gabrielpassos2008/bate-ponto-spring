dce9fadd628c7527593ea93c0892217de1c84f41
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-25 19:51:21 -0300

    - Altera o UsuarioCreateDTO adicionando o id do cargo.

--- src/main/java/com/gabriel/bate_ponto/dto/usuario/UsuarioCreateDTO.java
+++ src/main/java/com/gabriel/bate_ponto/dto/usuario/UsuarioCreateDTO.java
@@ -3,5 +3,5 @@ package com.gabriel.bate_ponto.dto.usuario;
 import com.gabriel.bate_ponto.model.Role;
 import com.gabriel.bate_ponto.model.Usuario;
 
-public record UsuarioCreateDTO(String email, String senha, String nome, boolean ativo, Role role) {
+public record UsuarioCreateDTO(String email, String senha, String nome, boolean ativo, Role role, int IdCargo) {
 }
