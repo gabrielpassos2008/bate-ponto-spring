71d6561a2c59c8bdad581a6017d799bfca6d0a10
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-25 19:52:12 -0300

    - Cria a Rota Post para registrar os cargos, no UsuarioController.

--- src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.gabriel.bate_ponto.controller;
 
 import com.gabriel.bate_ponto.Service.AdministradorService;
+import com.gabriel.bate_ponto.Service.CargoService;
+import com.gabriel.bate_ponto.dto.cargo.CargoCreateDTO;
+import com.gabriel.bate_ponto.dto.cargo.CargoResponseDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
+import com.gabriel.bate_ponto.model.Cargo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +23,10 @@ public class UsuarioController {
     @Autowired
     private AdministradorService administradorService;
 
-    @PostMapping("/registrar/adm")
+    @Autowired
+    private CargoService cargoService;
+
+    @PostMapping("/adiministrador/registrar/adm")
     //@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<UsuarioResponseDTO> postRegistrarAdm(@RequestBody UsuarioCreateDTO dto){
         UsuarioResponseDTO usuario = administradorService.registrarUsuario(dto);
@@ -28,4 +35,13 @@ public class UsuarioController {
                 .status(HttpStatus.CREATED)
                 .body(usuario);
     }
+
+    @PostMapping("/adiministrador/registrar/cargo")
+    public ResponseEntity<CargoResponseDTO> postCriarCargo(@RequestBody CargoCreateDTO dto){
+        CargoResponseDTO cargo = cargoService.registrarCargo(dto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(cargo);
+    }
+
 }
