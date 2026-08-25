32c4c1f4acd2fe8edf60caebc8c6a765b9746621
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-25 19:48:55 -0300

    - Cria o DTO Response e Create para o Cargo.

---
+++ src/main/java/com/gabriel/bate_ponto/dto/cargo/CargoCreateDTO.java
@@ -0,0 +1,4 @@
+package com.gabriel.bate_ponto.dto.cargo;
+
+public record CargoCreateDTO(String nome) {
+}
---
+++ src/main/java/com/gabriel/bate_ponto/dto/cargo/CargoResponseDTO.java
@@ -0,0 +1,4 @@
+package com.gabriel.bate_ponto.dto.cargo;
+
+public record CargoResponseDTO(Long id, String nome) {
+}
