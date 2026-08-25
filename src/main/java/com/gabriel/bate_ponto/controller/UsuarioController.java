031d02ab0ee1ebfeb91878fbc305644a4063023a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-25 17:23:40 -0300

    - Adiciona o PreAuthorize no controller.

--- src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class UsuarioController {
     private AdministradorService administradorService;
 
     @PostMapping("/registrar/adm")
+    //@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<UsuarioResponseDTO> postRegistrarAdm(@RequestBody UsuarioCreateDTO dto){
         UsuarioResponseDTO usuario = administradorService.registrarUsuario(dto);
 
