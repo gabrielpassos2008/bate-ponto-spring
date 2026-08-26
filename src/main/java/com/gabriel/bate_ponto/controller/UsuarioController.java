c62890af89403dad9ea8d41d53bcc4f9d3e2021b
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-26 19:47:34 -0300

    - Cria a rota para o Gestor listar os Cargos.

--- src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/UsuarioController.java
@@ -11,10 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ponto")
@@ -42,6 +41,13 @@ public class UsuarioController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(cargo);
+    }
+
+    @GetMapping("/gestor/listar/cargo")
+    public ResponseEntity<List<CargoResponseDTO>> getListarCargo(){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(cargoService.listarCargos());
     }
 
 }
