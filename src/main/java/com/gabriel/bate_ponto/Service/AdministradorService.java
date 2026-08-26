4040e2654e74dd87fbb7ad7249504cb6f122e020
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-26 19:59:36 -0300

    - Adiciona o método retornar cargo por id no registrar Usuário.

--- src/main/java/com/gabriel/bate_ponto/Service/AdministradorService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/AdministradorService.java
@@ -1,7 +1,9 @@
 package com.gabriel.bate_ponto.Service;
 
+import com.gabriel.bate_ponto.dto.cargo.CargoResponseDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
+import com.gabriel.bate_ponto.model.Cargo;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +21,20 @@ public class AdministradorService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private CargoService cargoService;
+
     public UsuarioResponseDTO registrarUsuario(UsuarioCreateDTO dto){
         this.usuarioService.validarEmailJaExiste(dto.email());
+        Cargo cargo = cargoService.retornarPorId(dto.IdCargo());
+
         Usuario novo = new Usuario();
         novo.setNome(dto.nome());
         novo.setEmail(dto.email());
         novo.setSenha(passwordEncoder.encode(dto.senha()));
         novo.setAtivo(dto.ativo());
         novo.setRole(dto.role());
+        novo.setCargo(cargo);
 
         this.usuarioRepository.save(novo);
 
