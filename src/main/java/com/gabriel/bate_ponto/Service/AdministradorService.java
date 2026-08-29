c69083fc28230fba522b9538b54439b111d72582
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-29 14:14:38 -0300

    - Altera o metodo registrarUsuario para cadastrar o adm com valores pre-setados.

--- src/main/java/com/gabriel/bate_ponto/Service/AdministradorService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/AdministradorService.java
@@ -1,9 +1,8 @@
 package com.gabriel.bate_ponto.Service;
 
-import com.gabriel.bate_ponto.dto.cargo.CargoResponseDTO;
-import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
-import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
-import com.gabriel.bate_ponto.model.Cargo;
+import com.gabriel.bate_ponto.dto.administrador.AdminCreateDTO;
+import com.gabriel.bate_ponto.dto.administrador.AdminResponseDTO;
+import com.gabriel.bate_ponto.model.Role;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,23 +20,20 @@ public class AdministradorService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private CargoService cargoService;
 
-    public UsuarioResponseDTO registrarUsuario(UsuarioCreateDTO dto){
+    public AdminResponseDTO registrarUsuario(AdminCreateDTO dto){
         this.usuarioService.validarEmailJaExiste(dto.email());
-        Cargo cargo = cargoService.retornarPorId(dto.IdCargo());
 
         Usuario novo = new Usuario();
         novo.setNome(dto.nome());
         novo.setEmail(dto.email());
         novo.setSenha(passwordEncoder.encode(dto.senha()));
-        novo.setAtivo(dto.ativo());
-        novo.setRole(dto.role());
-        novo.setCargo(cargo);
+        novo.setAtivo(true);
+        novo.setRole(Role.ROLE_ADMIN);
+        novo.setCargo(null);
 
         this.usuarioRepository.save(novo);
 
-        return new UsuarioResponseDTO(novo.getEmail(), novo.getNome(), novo.isAtivo(),novo.getRole());
+        return new AdminResponseDTO(novo.getNome(), novo.getEmail(), novo.getSenha());
     }
 }
