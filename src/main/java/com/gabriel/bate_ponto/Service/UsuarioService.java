6eb295083d68066acacf9ac9aa00158084304cad
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-30 20:00:58 -0300

    - Cria um novo método para retornar o usuário pelo email. - cria um novo método para retornar o usuário autenticado. - Adiciona a gestor no cadastro do colaborador.

--- src/main/java/com/gabriel/bate_ponto/Service/UsuarioService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/UsuarioService.java
@@ -3,7 +3,9 @@ package com.gabriel.bate_ponto.Service;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
 import com.gabriel.bate_ponto.exceptions.exceptions.EmailJaExisteException;
+import com.gabriel.bate_ponto.exceptions.exceptions.UsuarioNaoEncontradoException;
 import com.gabriel.bate_ponto.model.Cargo;
+import com.gabriel.bate_ponto.model.Role;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +24,39 @@ public class UsuarioService {
     @Autowired
     private CargoService cargoService;
 
+    @Autowired
+    private TokenService tokenService;
+
     public void validarEmailJaExiste(String email){
         if (usuarioRepository.existsByEmail(email)){
             throw new EmailJaExisteException();
         }
     }
 
-    public UsuarioResponseDTO registrarUsuario(UsuarioCreateDTO dto){
+    public UsuarioResponseDTO registrarColaborador(UsuarioCreateDTO dto){
+
         this.validarEmailJaExiste(dto.email());
         Cargo cargo = cargoService.retornarPorId(dto.IdCargo());
 
         Usuario novo = new Usuario();
+        novo.setGestor(retornarUsuarioAutenticado());
         novo.setNome(dto.nome());
         novo.setEmail(dto.email());
         novo.setSenha(passwordEncoder.encode(dto.senha()));
         novo.setAtivo(dto.ativo());
-        novo.setRole(dto.role());
+        novo.setRole(Role.ROLE_COLABORADOR);
         novo.setCargo(cargo);
 
         this.usuarioRepository.save(novo);
 
         return new UsuarioResponseDTO(novo.getEmail(), novo.getNome(), novo.isAtivo(),novo.getRole());
+    }
+
+    public Usuario retornarPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(UsuarioNaoEncontradoException::new);
+    }
+
+    public Usuario retornarUsuarioAutenticado(){
+        return retornarPorEmail(tokenService.retornarEmailDoToken());
     }
 }
