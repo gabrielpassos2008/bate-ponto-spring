a369fa6a6bada790e82f0c46de32a961dccfff56
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-01 12:42:02 -0300

    - Cria a nova classe ColaboradorController.

---
+++ src/main/java/com/gabriel/bate_ponto/controller/ColaboradorController.java
@@ -0,0 +1,9 @@
+package com.gabriel.bate_ponto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ponto/gestor")
+public class ColaboradorController {
+}
