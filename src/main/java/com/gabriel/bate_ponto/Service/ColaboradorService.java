783f2cbecef9bde87e5efea7ab5464f4ec356087
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-03 20:16:17 -0300

    - Cria o novo metodo listaDeUsuarioPeloGestor.

--- src/main/java/com/gabriel/bate_ponto/Service/ColaboradorService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/ColaboradorService.java
@@ -2,6 +2,7 @@ package com.gabriel.bate_ponto.Service;
 
 import com.gabriel.bate_ponto.dto.usuario.UsuarioCreateDTO;
 import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
+import com.gabriel.bate_ponto.exceptions.exceptions.UsuarioNaoEncontradoException;
 import com.gabriel.bate_ponto.model.Cargo;
 import com.gabriel.bate_ponto.model.Role;
 import com.gabriel.bate_ponto.model.Usuario;
@@ -9,6 +10,8 @@ import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ColaboradorService {
@@ -43,6 +46,12 @@ public class ColaboradorService {
 
         this.usuarioRepository.save(novo);
 
-        return new UsuarioResponseDTO(novo.getEmail(), novo.getNome(), novo.isAtivo(),novo.getRole());
+        return new UsuarioResponseDTO(novo.getEmail(), novo.getNome(), novo.isAtivo(),novo.getRole(),novo.getCargo());
+    }
+
+    public List<UsuarioResponseDTO> listaDeUsuarioPeloGestor(){
+        Usuario usuario = usuarioService.retornarUsuarioAutenticado();
+        return usuarioRepository.findByRoleAndGestor(Role.ROLE_COLABORADOR,usuario).orElseThrow(UsuarioNaoEncontradoException::new)
+                .stream().map(Usuario -> new UsuarioResponseDTO(usuario.getEmail(), usuario.getNome(), usuario.isAtivo(), usuario.getRole(),usuario.getCargo())).toList();
     }
 }
