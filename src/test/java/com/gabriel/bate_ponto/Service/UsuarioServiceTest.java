469f1ba3e791e389f2e1d086053d4e1bf3f155a2
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-08 17:23:05 -0300

    - Inicia o metodo de validar pesquisa de usuario. No UsuarioServiceTest

--- src/test/java/com/gabriel/bate_ponto/Service/UsuarioServiceTest.java
+++ src/test/java/com/gabriel/bate_ponto/Service/UsuarioServiceTest.java
@@ -1,7 +1,10 @@
 package com.gabriel.bate_ponto.Service;
 
+import com.gabriel.bate_ponto.dto.pesquisa.PesquisaUsuarioDTO;
+import com.gabriel.bate_ponto.dto.usuario.UsuarioResponseDTO;
 import com.gabriel.bate_ponto.exceptions.exceptions.EmailJaExisteException;
 import com.gabriel.bate_ponto.exceptions.exceptions.UsuarioNaoEncontradoException;
+import com.gabriel.bate_ponto.model.Cargo;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,14 +33,26 @@ public class UsuarioServiceTest {
 
     private Usuario usuario;
 
+    private Usuario gestor;
+
+    private Cargo cargo;
+
     @BeforeEach
     void setUp(){
+        gestor = new Usuario();
+        gestor.setNome("gestor");
+
+        cargo = new Cargo();
+        cargo.setId(1L);
+        cargo.setNome("cargo");
+
         usuario = new Usuario();
         usuario.setId(1L);
         usuario.setNome("teste");
         usuario.setEmail("teste@gmail.com");
         usuario.setSenha("teste");
         usuario.setAtivo(true);
+        usuario.setGestor(gestor);
     }
 
     @Test
@@ -54,16 +70,25 @@ public class UsuarioServiceTest {
     }
 
     @Test
-    void retornarPorEmail_quandoExiste(){
+    void validarRetornarPorEmail_quandoExiste(){
         when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.of(usuario));
         Usuario resultado = usuarioService.retornarPorEmail(usuario.getEmail());
         assertEquals(resultado,usuario);
         verify(usuarioRepository).findByEmail(usuario.getEmail());
     }
+
     @Test
-    void retornarPorEmail_quandoNaoExiste(){
+    void validarRetornarPorEmail_quandoNaoExiste(){
         when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.empty());
         assertThrows(UsuarioNaoEncontradoException.class,() -> usuarioService.retornarPorEmail(usuario.getEmail()));
         verify(usuarioRepository).findByEmail(usuario.getEmail());
+    }
+
+    @Test
+    void validarPesquisarUsuario_quandoExiste(){
+        List<Usuario> listaUsuario = List.of(usuario);
+        when(usuarioRepository.findByGestorAndCargoAndNomeContainingIgnoreCase(gestor,cargo,"teste")).thenReturn(listaUsuario);
+        List<UsuarioResponseDTO> lista = usuarioService.pesquisarUsuario(new PesquisaUsuarioDTO("teste",cargo.getId()));
+
     }
 }
