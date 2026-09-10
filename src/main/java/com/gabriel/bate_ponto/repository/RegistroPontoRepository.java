84221cffe0734521fadbcbe9d8ea1f66017628bd
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-10 20:57:17 -0300

    - Cria o método aonde vai retornar o último ponto pelo usuário e pela data, ordenando pela hora.

--- src/main/java/com/gabriel/bate_ponto/repository/RegistroPontoRepository.java
+++ src/main/java/com/gabriel/bate_ponto/repository/RegistroPontoRepository.java
@@ -1,7 +1,14 @@
 package com.gabriel.bate_ponto.repository;
 
 import com.gabriel.bate_ponto.model.RegistroPonto;
+import com.gabriel.bate_ponto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface RegistroPontoRepository extends JpaRepository<RegistroPonto,Long> {
+
+    RegistroPonto findTopByUsuarioAndDataOrderByHoraDesc( Usuario usuario, LocalDate data);
+
+    boolean existsByDataAndUsuario(LocalDate data, Usuario usuario);
 }
