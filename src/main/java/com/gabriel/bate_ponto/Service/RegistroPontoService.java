aaad2754462fbb825510e9d70854cfc843576cca
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-10 20:40:43 -0300

    - Cria o método validarSeExistePonto no RegistroPontoService.

--- src/main/java/com/gabriel/bate_ponto/Service/RegistroPontoService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/RegistroPontoService.java
@@ -3,8 +3,8 @@ package com.gabriel.bate_ponto.Service;
 import com.gabriel.bate_ponto.Service.usuarios.UsuarioService;
 import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoResponse;
 import com.gabriel.bate_ponto.model.RegistroPonto;
+import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.RegistroPontoRepository;
-import com.gabriel.bate_ponto.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,4 +38,13 @@ public class RegistroPontoService {
                 ponto.getTipo());
     }
 
+    public Boolean validarSeExistePonto(){
+        Usuario usuario = usuarioService.retornarUsuarioAutenticado();
+        LocalDate data = LocalDate.now();
+        return pontoRepository.existsByDataAndUsuario(data,usuario))
+            //returna verdadeiro se existe um ponto no dia atual e para o usuario logado.
+    }
+
+
 }
+
