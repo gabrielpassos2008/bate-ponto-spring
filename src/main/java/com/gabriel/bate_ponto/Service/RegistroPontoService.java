b0ae121d64e1bc5b7584d210228ba2543fde9573
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-10 21:05:18 -0300

    - Cria o método aonde vai retorna o tipo do ponto. - Cria o método aonde vai validar se existe ponto nesse dia, adicionando no RetornarTipo.

--- src/main/java/com/gabriel/bate_ponto/Service/RegistroPontoService.java
+++ src/main/java/com/gabriel/bate_ponto/Service/RegistroPontoService.java
@@ -2,6 +2,7 @@ package com.gabriel.bate_ponto.Service;
 
 import com.gabriel.bate_ponto.Service.usuarios.UsuarioService;
 import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoResponse;
+import com.gabriel.bate_ponto.exceptions.exceptions.PontoJaRegistradoException;
 import com.gabriel.bate_ponto.model.RegistroPonto;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.RegistroPontoRepository;
@@ -24,13 +25,14 @@ public class RegistroPontoService {
 
         LocalDate data = LocalDate.now();
         LocalTime hora = LocalTime.now();
+        Usuario usuario = usuarioService.retornarUsuarioAutenticado();
 
         RegistroPonto ponto = new RegistroPonto();
         ponto.setData(data);
         ponto.setHora(hora);
         ponto.setOrigem("Web");
-        ponto.setTipo("entrada");
-        ponto.setUsuario(usuarioService.retornarUsuarioAutenticado());
+        ponto.setTipo(retornarTipo(usuario,data));
+        ponto.setUsuario(usuario);
         pontoRepository.save(ponto);
         return new RegistroPontoResponse(
                 ponto.getData(),
@@ -38,12 +40,34 @@ public class RegistroPontoService {
                 ponto.getTipo());
     }
 
-    public Boolean validarSeExistePonto(){
-        Usuario usuario = usuarioService.retornarUsuarioAutenticado();
-        LocalDate data = LocalDate.now();
-        return pontoRepository.existsByDataAndUsuario(data,usuario))
-            //returna verdadeiro se existe um ponto no dia atual e para o usuario logado.
+    public void validarSeExistePonto(Usuario usuario, LocalDate data){
+        if(pontoRepository.existsByDataAndUsuarioAndTipo(data,usuario, "Saída")){
+            throw new PontoJaRegistradoException();
+        }
     }
+
+    public String retornarTipo(Usuario usuario, LocalDate data){
+        this.validarSeExistePonto(usuario,data);
+        RegistroPonto ponto = pontoRepository.findTopByUsuarioAndDataOrderByHoraDesc(usuario,data);
+        if (ponto == null){
+            return "Início";
+        } else if (ponto.getTipo().equals("Início")) {
+            return "Intervalo";
+        } else if (ponto.getTipo().equals("Intervalo")) {
+            return "Fim Intervalo";
+        }
+        return "Saída";
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 }
