6771d84dea2e443f203d3c0d9bbf8a3ed485d6f9
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-11 22:40:38 -0300

    - Cria a rota get para listar os registro de ponto.

--- src/main/java/com/gabriel/bate_ponto/controller/ColaboradorController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/ColaboradorController.java
@@ -6,9 +6,12 @@ import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ponto/gestor")
@@ -22,5 +25,11 @@ public class ColaboradorController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(pontoService.registrarPonto());
+    }
+    @GetMapping("/listar/registros")
+    public ResponseEntity<List<RegistroPontoResponse>> getListarPontos(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(pontoService.listarPontoPorDia());
     }
 }
