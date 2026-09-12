94413049bd1f14c786f83b608a55946c1e05fefc
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-11 22:23:29 -0300

    - Cria a rota post para bater no ponto no ColaboradorController.

--- src/main/java/com/gabriel/bate_ponto/controller/ColaboradorController.java
+++ src/main/java/com/gabriel/bate_ponto/controller/ColaboradorController.java
@@ -1,9 +1,26 @@
 package com.gabriel.bate_ponto.controller;
 
+import com.gabriel.bate_ponto.Service.RegistroPontoService;
+import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoCreate;
+import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ponto/gestor")
 public class ColaboradorController {
+
+    @Autowired
+    private RegistroPontoService pontoService;
+
+    @PostMapping("/bater")
+    public ResponseEntity<RegistroPontoResponse> postBaterPonto(){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(pontoService.registrarPonto());
+    }
 }
