c7ace6a679f0ff4bd60f682100e842bcaac754d3
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-11 22:35:30 -0300

    - Cria o método que fara a consulta no banco de dados e retornaras os registros de ponto daquele dia e daquele usuário.

--- src/main/java/com/gabriel/bate_ponto/repository/RegistroPontoRepository.java
+++ src/main/java/com/gabriel/bate_ponto/repository/RegistroPontoRepository.java
@@ -5,10 +5,14 @@ import com.gabriel.bate_ponto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface RegistroPontoRepository extends JpaRepository<RegistroPonto,Long> {
 
     RegistroPonto findTopByUsuarioAndDataOrderByHoraDesc( Usuario usuario, LocalDate data);
+
+    Optional<List<RegistroPonto>> findByUsuarioAndData(Usuario usuario, LocalDate data);
 
     boolean existsByDataAndUsuarioAndTipo(LocalDate data, Usuario usuario, String tipo);
 }
