3f248c67f8daf5e3fc6f28a2c7af034afec553cb
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-16 19:55:15 -0300

    - Adiciona o PontoNaoRegistradoException no GlobalExceptionHandler.

--- src/main/java/com/gabriel/bate_ponto/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gabriel/bate_ponto/exceptions/GlobalExceptionHandler.java
@@ -31,6 +31,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body(erro);
     }
+    @ExceptionHandler(PontoNaoRegistradoException.class)
+    public ResponseEntity<MensagemErroDTO> pontoNaoRegistrado (PontoNaoRegistradoException exception){
+        MensagemErroDTO erro = new MensagemErroDTO(
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.name());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(erro);
+    }
 
     @ExceptionHandler(CargoJaExisteException.class)
     public ResponseEntity<MensagemErroDTO> cargoJaExiste(CargoJaExisteException exception){
