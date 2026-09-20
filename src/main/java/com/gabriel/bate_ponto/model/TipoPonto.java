7499da9ca2489f2a5630f2476bd6a4f88d8bcda8
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-19 23:29:42 -0300

    - Cria uma Enum para ter os tipos de pontos pre determinados.

---
+++ src/main/java/com/gabriel/bate_ponto/model/TipoPonto.java
@@ -0,0 +1,10 @@
+package com.gabriel.bate_ponto.model;
+
+
+// enum serve para representar valores que nao mudam, como a role, os cargos e tambem os valores do ponto
+public enum TipoPonto {
+    INICIO,
+    INTERVALO,
+    FIM_INTERVALO,
+    SAIDA
+}
