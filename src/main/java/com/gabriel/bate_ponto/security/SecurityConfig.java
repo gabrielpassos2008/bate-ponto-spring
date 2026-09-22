904477aed344fe92907bece8e947c25fa8134a0a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-22 16:48:43 -0300

    - Libera a rota do swagger no SecurityConfig.

--- src/main/java/com/gabriel/bate_ponto/security/SecurityConfig.java
+++ src/main/java/com/gabriel/bate_ponto/security/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/ponto/administrador/registrar/adm").permitAll()
                         .requestMatchers("/ponto/adiministrador/**").hasRole("ADMIN")
                         .requestMatchers("/ponto/gestor/**").hasRole("GESTOR")
