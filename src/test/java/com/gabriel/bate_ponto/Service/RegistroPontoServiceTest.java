6cb11611fdfc1cdda8b4fb2ff8491834e2a6199b
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-23 19:55:05 -0300

    - Cria novo test para validarSeDiaJaFinalizado_quandoRetornaException no RegistroPontoServiceTest.

--- src/test/java/com/gabriel/bate_ponto/Service/RegistroPontoServiceTest.java
+++ src/test/java/com/gabriel/bate_ponto/Service/RegistroPontoServiceTest.java
@@ -1,9 +1,14 @@
 package com.gabriel.bate_ponto.Service;
 
+import com.gabriel.bate_ponto.Service.usuarios.UsuarioService;
+import com.gabriel.bate_ponto.dto.registroPonto.RegistroPontoResponse;
+import com.gabriel.bate_ponto.exceptions.exceptions.ponto.PontoJaRegistradoException;
 import com.gabriel.bate_ponto.model.RegistroPonto;
+import com.gabriel.bate_ponto.model.TipoPonto;
 import com.gabriel.bate_ponto.model.Usuario;
 import com.gabriel.bate_ponto.repository.RegistroPontoRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,6 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class RegistroPontoServiceTest {
@@ -18,41 +27,58 @@ public class RegistroPontoServiceTest {
     @Mock
     private RegistroPontoRepository repository;
 
+    @Mock
+    private UsuarioService usuarioService;
+
     @InjectMocks
     private RegistroPontoService service;
 
+    private RegistroPonto inicio;
+    private RegistroPonto intervalo;
+    private RegistroPonto fimIntervalo;
+    private RegistroPonto saida;
+    private Usuario usuario;
+
     @BeforeEach
     void setUp(){
-        Usuario usuario = new Usuario();
+        usuario = new Usuario();
         usuario.setId(1L);
         usuario.setNome("teste");
 
-        RegistroPonto inicio = new RegistroPonto();
+        inicio = new RegistroPonto();
         inicio.setId(1L);
         inicio.setData(LocalDate.now());
         inicio.setHora(LocalTime.of(8,30));
-        inicio.setTipo("Início");
+        inicio.setTipo(TipoPonto.INICIO);
         inicio.setOrigem("web");
 
-        RegistroPonto intervalo = new RegistroPonto();
+        intervalo = new RegistroPonto();
         intervalo.setId(2L);
         intervalo.setData(LocalDate.now());
         intervalo.setHora(LocalTime.of(12,30));
-        intervalo.setTipo("Intervalo");
+        intervalo.setTipo(TipoPonto.INTERVALO);
         intervalo.setOrigem("web");
 
-        RegistroPonto fimIntervalo = new RegistroPonto();
+        fimIntervalo = new RegistroPonto();
         fimIntervalo.setId(3L);
         fimIntervalo.setData(LocalDate.now());
         fimIntervalo.setHora(LocalTime.of(13,30));
-        fimIntervalo.setTipo("Fim Intervalo");
+        fimIntervalo.setTipo(TipoPonto.FIM_INTERVALO);
         fimIntervalo.setOrigem("web");
 
-        RegistroPonto saida = new RegistroPonto();
+        saida = new RegistroPonto();
         saida.setId(4L);
         saida.setData(LocalDate.now());
         saida.setHora(LocalTime.of(17,30));
-        saida.setTipo("Saída");
+        saida.setTipo(TipoPonto.FIM_INTERVALO);
         saida.setOrigem("web");
     }
+
+    @Test
+    void validarSeDiaJaFinalizado_quandoRetornaException(){
+        when(repository.existsByDataAndUsuarioAndTipo(LocalDate.now(),usuario,TipoPonto.SAIDA)).thenReturn(true);
+        assertThrows(PontoJaRegistradoException.class,()-> service.validarSeDiaJaFinalizado(usuario,LocalDate.now()));
+        verify(repository).existsByDataAndUsuarioAndTipo(LocalDate.now(),usuario,TipoPonto.SAIDA);
+    }
+
 }
