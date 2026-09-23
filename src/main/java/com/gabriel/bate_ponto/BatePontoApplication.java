1ae930ec233b646ddf109b0c893327f3c08da698
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-09-23 17:07:44 -0300

    - Criando o BancoHorasService, SchedulingConfig. - Adicionando "@EnableScheduling" no Application.

--- src/main/java/com/gabriel/bate_ponto/BatePontoApplication.java
+++ src/main/java/com/gabriel/bate_ponto/BatePontoApplication.java
@@ -2,8 +2,10 @@ package com.gabriel.bate_ponto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BatePontoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/gabriel/bate_ponto/Service/BancoHorasService.java
@@ -0,0 +1,7 @@
+package com.gabriel.bate_ponto.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BancoHorasService {
+}
